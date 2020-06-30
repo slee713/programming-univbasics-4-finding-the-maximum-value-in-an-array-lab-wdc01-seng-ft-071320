0fdf4885b7e7bf3c62375da25808bd8baa0f0e46
Steven Lee <steven.lee1393@gmail.com>
2020-06-30 18:32:35 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -4,7 +4,7 @@ def find_max_value(array)
   # max_value=sorted_array[-1]
 
   i=0
-  max_value = nil
+  max_value = 0
   while i<array.length do
     if array[i] > max_value
       max_value=array[i]
